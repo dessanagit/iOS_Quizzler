b44ef3aeeacb0afdf37ccd5370322ac4b4eb42b3
dessanagit <dessanacms@gmail.com>
2021-08-10 20:46:07 -0300

    Show Questions

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -10,11 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    //Labels of views
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    let quiz = ["Four + Two is equal to six",
+    "Five - Three is greater than one",
+    "Three + Eight is lesser than Ten"]
+
+    var questionNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // 2* - call function to always update on viewLoad when triggered
+        updateUI()
+       
+   }
+
+    //Actions
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        questionNumber += 1 // questionNumber = + 1
+    // 2* - call function to update INDEX NUMBER as it increases, inside the array
+        updateUI()
     }
-
-
+    
+    // 1* - function to update UI - Need this functionality many times to update the questions (viewDidLoad() will capture this) and ALSO increases the question number.
+    func updateUI() {
+        questionLabel.text = quiz[questionNumber]
+    }
+    
 }
 
