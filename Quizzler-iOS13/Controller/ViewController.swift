53a6efaa2f43404c1a8b581c53a634df7ff2511b
dessanagit <dessanacms@gmail.com>
2021-09-02 11:52:40 -0400

    Implement MVC

--- Quizzler-iOS13/Controller/ViewController.swift
+++ Quizzler-iOS13/Controller/ViewController.swift
@@ -15,26 +15,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var progressBar: UIProgressView!
     @IBOutlet weak var trueButton: UIButton!
     @IBOutlet weak var falseButton: UIButton!
-    
-    
-    // ---- Substituting the 2D Array -> Collection with a Structure
-    let quiz = [
-        Question(q: "A slug's blood is green.", a: "True"),
-                Question(q: "Approximately one quarter of human bones are in the feet.", a: "True"),
-                Question(q: "The total surface area of two human lungs is approximately 70 square metres.", a: "True"),
-                Question(q: "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", a: "True"),
-                Question(q: "In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.", a: "False"),
-                Question(q: "It is illegal to pee in the Ocean in Portugal.", a: "True"),
-                Question(q: "You can lead a cow down stairs but not up stairs.", a: "False"),
-                Question(q: "Google was originally called 'Backrub'.", a: "True"),
-                Question(q: "Buzz Aldrin's mother's maiden name was 'Moon'.", a: "True"),
-                Question(q: "The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.", a: "False"),
-                Question(q: "No piece of square dry paper can be folded in half more than 7 times.", a: "False"),
-                Question(q: "Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog.", a: "True")
-]
 
-    //// Variable to help keep tracking quiz collection
-    var questionNumber = 0
+    // Variable to call the structure QuizBrain in order to use it in the code (below).
+   var quizBrain = QuizBrain()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -49,10 +32,13 @@ class ViewController: UIViewController {
         
         // ---- Create If Statement to check answers
        
-        var userAnswer = sender.currentTitle //True, False
-        // var actualQuestion = quiz[questionNumber]
-        var actualAnswer = quiz[questionNumber].answer //accessing answer
-                            //// quiz[questionNumber] -> Index of each Question(), inside the collection ( [] ), followed by the access of the respective answer (property).
+        var userAnswer = sender.currentTitle! //True, False
+                                            /// Because it known for sure there's an answer(values), it's ok to force to open it.
+        
+        //// using the structure to use the checkin logic. userAnswer is actually the variable seens above.
+        quizBrain.checkAnswer(userAnswer) //It's not necessary to express the external property, because the output in the method is _.
+        
+        
         //Checking answers
         if userAnswer == actualAnswer {
             sender.backgroundColor = .green ////changes button color to green
